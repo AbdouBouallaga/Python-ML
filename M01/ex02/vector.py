ea90c4ec9ace22dfeb9c12bfc13721302d8cac61
Bouallaga Abdelkader <babdelka@e2r12p3.1337.ma>
2022-03-06 19:02:19 +0100

    update

--- M01/ex02/vector.py
+++ M01/ex02/vector.py
@@ -57,9 +57,76 @@ class Vector:
             return ret
         else:
             print("not same shape")
-            
 
-v1 = Vector([[0.0,1.0,2.0]])
+    def __add__(self, vec2):
+        ret = []
+        if self.shape == vec2.shape:
+            if any(isinstance(i, list) for i in self.values):
+                for i, j in zip(self.values, vec2.values):
+                    ret.append([i[0] + j[0]])
+            else:
+                for i, j in zip(self.values, vec2.values):
+                    ret.append(i + j)
+            return(Vector(ret))
+        else:
+            print("not same shape")
+            return(Vector(0))
+    
+    def __sub__(self, vec2):
+         ret = []
+         if self.shape == vec2.shape:
+             if any(isinstance(i, list) for i in self.values):
+                 for i, j in zip(self.values, vec2.values):
+                     ret.append([i[0] - j[0]])
+             else:
+                 for i, j in zip(self.values, vec2.values):
+                     ret.append(i - j)
+             return(Vector(ret))
+         else:
+             print("not same shape")
+             return(Vector(0))
+
+    def __radd__(self, vec2):
+        return(vec2.__add__(self))
+
+    def __rsub__(self, vec2):
+        return(vec2.__sub__(self))
+
+    def __truediv__(self, scal):
+        ret = []
+        if type(scal) is int or type(scal) is float:
+            if any(isinstance(i, list) for i in self.values):
+                for i in self.values:
+                    ret.append([i[0] / scal])
+            else:
+                for i in self.values:
+                    ret.append(i / scal)
+            return(Vector(ret))
+        else:
+            print("not a scalar")
+            return(Vector(0))
+
+    #    def __rtruediv__() ? scalar
+    
+    def __mul__(self, scal):
+        ret = []
+        if type(scal) is int or type(scal) is float:
+            if any(isinstance(i, list) for i in self.values):
+                for i in self.values:
+                    ret.append([i[0] * scal])
+            else:
+                for i in self.values:
+                    ret.append(i * scal)
+            return(Vector(ret))
+        else:
+            print("not a scalar")
+            return(Vector(0))
+
+
+
+v1 = Vector([2.0,5.0,6.0])
+v1s = Vector([1.0,2.0,4.0])
+v1n = Vector([2.0,5.0])
 
 print("print v1 values\n", v1.values)
 print("print v1 shape\n", v1.shape)
@@ -82,6 +149,25 @@ print("print transpose back v4\n", v4tb.values)
 print("dot product of v2 and v2, same shape:", v2.dot(v2))
 print()
 print("dot product of v4t and v2 not same shape:", v4t.dot(v2))
+print()
+vplus = Vector(1)
+vplus = v1 + v1n
+print("not nested: v1 + v1n =", vplus.values)
 
-
-
+vplus = v1 + v1
+vn = Vector([[2.0], [5.0], [6.0]])
+print("here is v1 and vn\nv1",v1.values, "\nvn", vn.values)
+print()
+print("not nested: v1 + v1 =", vplus.values)
+print()
+vplus = vn.__radd__(vn)
+print("nested: vn + vn =", vplus.values)
+print()
+vsub = v1.__rsub__(v1s)
+print("non nested: v1 - v1s =", vsub.values)
+print()
+vdiv = v1 / 2
+print("div: v1 / 2 =", vdiv.values)
+print()
+vmul = v1 * 2
+print("multi: v1 * 2 =", vmul.values)
